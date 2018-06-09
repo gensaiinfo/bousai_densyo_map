de3c8252437c5f0bf2e0651c5d7d9c5b689fcb8a
HeRoMo <hero@asterisk-works.jp>
2018-06-09 10:01:25 +0900

    fix lint errors and refactoring

--- src/worker.js
+++ src/worker.js
@@ -1,40 +1,24 @@
-'use strict'
+/* eslint no-restricted-globals: 1 */
 
-const CACHE = 'CACHE-V1'
-const TIMEOUT=400;
+const CACHE = 'CACHE-V1';
+const TIMEOUT = 400;
 
 const PRECACHE_URLS = [
-  "/bousai_densyo_map/",
-  "/bousai_densyo_map/styles/bundle.css",
-  "/bousai_densyo_map/bundle.js",
-  "/bousai_densyo_map/jsons/saigai_densyo.json",
-  "/bousai_densyo_map/fonts/fontawesome-webfont.woff2",
-  "/bousai_densyo_map/jsons/japan_prefs.topojson",
-  "/bousai_densyo_map/jsons/japan.topojson",
-]
-
-self.addEventListener('install', (event)=>{
-  console.info("Installing Service Worker")
-  event.waitUntil(precache())
-})
-
-self.addEventListener('fetch', (event)=>{
-  // ネットワークから取得を試み、なければキャッシュを使う
-  if(/bousai_densyo_map/.test(event.request.url)){
-    event.respondWith(fromNetwork(event.request, TIMEOUT).catch(
-      ()=>fromCache(CACHE, event.request)
-    ))
-  }
-})
+  '/bousai_densyo_map/',
+  '/bousai_densyo_map/styles/bundle.css',
+  '/bousai_densyo_map/bundle.js',
+  '/bousai_densyo_map/jsons/saigai_densyo.json',
+  '/bousai_densyo_map/fonts/fontawesome-webfont.woff2',
+  '/bousai_densyo_map/jsons/japan_prefs.topojson',
+  '/bousai_densyo_map/jsons/japan.topojson',
+];
 
 /**
  * サイトコンテンツをプリキャッシュする。
  * @return [Promise]
  */
-function precache(){
-  return caches.open(CACHE).then((cache)=>{
-    return cache.addAll(PRECACHE_URLS);
-  })
+function precache() {
+  return caches.open(CACHE).then(cache => cache.addAll(PRECACHE_URLS));
 }
 
 /**
@@ -44,9 +28,9 @@ function precache(){
  * @return [Promise] 取得したコンテンツを渡されたPromise
  */
 function fromNetwork(request, timeout) {
-  return new Promise( (resolve, reject)=> {
-    let timeoutId = setTimeout(reject, timeout);
-    fetch(request).then( (response)=> {
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then((response) => {
       clearTimeout(timeoutId);
       resolve(response);
     }, reject);
@@ -59,9 +43,20 @@ function fromNetwork(request, timeout) {
  * @return [Response]  キャッシュから取得したレスポンスオブジェクト
  */
 function fromCache(cacheName, request) {
-  return caches.open(cacheName).then((cache)=>{
-    return cache.match(request).then( (matching)=> {
-      return matching || Promise.reject('no-match');
-    });
-  });
+  return caches.open(cacheName).then(cache => (
+    cache.match(request).then(matching => (
+      matching || Promise.reject(new Error('no-match'))))));
 }
+
+self.addEventListener('install', (event) => {
+  // console.info('Installing Service Worker');
+  event.waitUntil(precache());
+});
+
+self.addEventListener('fetch', (event) => {
+  // ネットワークから取得を試み、なければキャッシュを使う
+  if (/bousai_densyo_map/.test(event.request.url)) {
+    event.respondWith(fromNetwork(event.request, TIMEOUT)
+      .catch(() => fromCache(CACHE, event.request)));
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,60 @@
-const webpack =  require('webpack') ;
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  context: __dirname + '/src',
+  context: `${__dirname}/src`,
   entry: {
-    bundle: "./entry.js"
+    bundle: './entry.js',
   },
   output: {
-    path: __dirname + "/docs",
-    filename: "[name].js"
+    path: `${__dirname}/docs`,
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules|worker.js)/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
-      {test: /\.html$/, loader: "file-loader?name=[path][name].[ext]" },
+      { test: /\.html$/, loader: 'file-loader?name=[path][name].[ext]' },
       {
         test: /\.(css|scss)$/,
-        use:[ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
-      {test: /\.(png|jpg)$/, exclude: /icons/, loader: "file-loader?name=images/[name].[ext]&publicPath=../"},
-      {test: /\.(woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?name=fonts/[name].[ext]&publicPath=../"},
-      {test: /.*worker.js/, loader: "file-loader?name=[name].[ext]"},
-      {test: /icons\/.+.(png|svg|xml|ico)/, loader: "file-loader?name=icons/[name].[ext]"}
-    ]
+      { test: /\.(png|jpg)$/, exclude: /icons/, loader: 'file-loader?name=images/[name].[ext]&publicPath=../' },
+      { test: /\.(woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?name=fonts/[name].[ext]&publicPath=../' },
+      { test: /.*worker.js/, loader: 'file-loader?name=[name].[ext]' },
+      { test: /icons\/.+.(png|svg|xml|ico)/, loader: 'file-loader?name=icons/[name].[ext]' },
+    ],
   },
-  resolve:{
+  resolve: {
     extensions: ['.html', '.js', '.json', '.scss', '.css'],
-    alias: {
-      leaflet_marker: __dirname + "/node_modules/leaflet/dist/images/marker-icon.png",
-      leaflet_marker_2x: __dirname + "/node_modules/leaflet/dist/images/marker-icon-2x.png",
-      leaflet_marker_shadow: __dirname + "/node_modules/leaflet/dist/images/marker-shadow.png",
-      leaflet_css: __dirname + "/node_modules/leaflet/dist/leaflet.css",
-      font_awesome_css: __dirname + "/node_modules/font-awesome/css/font-awesome.css",
-      chrome192: __dirname+"/icons/android-chrome-192x192.png",
-      chrome384: __dirname+"/icons/android-chrome-384x384.png",
-      apple_icon: __dirname+"/icons/apple-touch-icon.png",
-      favicon16: __dirname+"/icons/favicon-16x16.png",
-      favicon32: __dirname+"/icons/favicon-32x32.png",
-      favicon: __dirname+"/icons/favicon.ico",
-      browserconfig: __dirname+"/icons/browserconfig.xml",
-      safari_pinned: __dirname+"/icons/safari-pinned-tab.svg",
-      mstile: __dirname+"/icons/mstile-150x150.png"
-    }
   },
-  devtool: "source-map",
-  plugins:[
+  devtool: 'source-map',
+  plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles/[name].css",
-      chunkFilename: "styles/[id].css"
+      filename: 'styles/[name].css',
+      chunkFilename: 'styles/[id].css',
     }),
     new CopyWebpackPlugin([
       { from: 'manifest.json', to: 'manifest.json' },
-      { from: 'data/japan.topojson', to: 'jsons/', context: '../'},
+      { from: 'data/japan.topojson', to: 'jsons/', context: '../' },
       { from: 'data/japan_prefs.topojson', to: 'jsons/', context: '../' },
       { from: 'data/saigai_densyo.json', to: 'jsons/', context: '../' },
-    ])
+      { from: 'icons/*.png', to: './', context: '../' },
+      { from: 'icons/*.ico', to: './', context: '../' },
+      { from: 'icons/*.xml', to: './', context: '../' },
+      { from: 'icons/*.svg', to: './', context: '../' },
+    ]),
   ],
   devServer: {
-    contentBase: __dirname + "/docs",
+    contentBase: `${__dirname}/docs`,
     compress: true,
     host: '0.0.0.0',
     port: 9000,
-    hot:true,
+    hot: true,
     open: true,
-    publicPath: "/bousai_densyo_map/"
-  }
-}
+    publicPath: '/bousai_densyo_map/',
+  },
+};
